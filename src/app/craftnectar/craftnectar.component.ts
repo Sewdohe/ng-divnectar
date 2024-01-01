013e65dfee7a165b81b3bfafd0cd8ad197ddde0a
Sewdohe <sewdohe@protonmail.com>
2023-12-31 23:31:45 -0500

    Better theme

--- src/app/craftnectar/craftnectar.component.ts
+++ src/app/craftnectar/craftnectar.component.ts
@@ -3,12 +3,15 @@ import { CraftnectarDataService } from '../craftnectar-data.service';
 import { Observable } from 'rxjs';
 import { PlayerlistResponse } from '../../types';
 import { CommonModule } from '@angular/common';
+import {MatCardModule} from '@angular/material/card';
+
 
 @Component({
   selector: 'app-craftnectar',
   standalone: true,
   imports: [
-    CommonModule
+    CommonModule,
+    MatCardModule
   ],
   templateUrl: './craftnectar.component.html',
   styleUrl: './craftnectar.component.scss'
