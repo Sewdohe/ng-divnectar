591386acb3d65fbb2031061c3ffc57f797e1721f
Sewdohe <sewdohe@protonmail.com>
2024-01-02 11:35:13 -0500

    added some content to server page.
    Working on nested navigation links.

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,6 +1,6 @@
 import { Component, inject } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
-import { AsyncPipe } from '@angular/common';
+import { AsyncPipe, CommonModule } from '@angular/common';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
@@ -9,6 +9,15 @@ import { MatIconModule } from '@angular/material/icon';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
 import { RouterOutlet, RouterLink } from '@angular/router';
+import {MatTreeModule} from '@angular/material/tree';
+
+
+interface MenuNode {
+  name: string,
+  icon: string,
+  route: string,
+  children?: MenuNode[]
+}
 
 @Component({
   selector: 'app-nav',
@@ -23,11 +32,37 @@ import { RouterOutlet, RouterLink } from '@angular/router';
     MatIconModule,
     AsyncPipe,
     RouterOutlet,
-    RouterLink
+    RouterLink,
+    CommonModule,
+    MatTreeModule
   ]
 })
 export class NavComponent {
   private breakpointObserver = inject(BreakpointObserver);
+  navigationItems: MenuNode[] = [
+    {
+      name: 'Home',
+      icon: 'home',
+      route: '',
+    },
+    {
+      name: 'CraftNectar',
+      icon: 'dns',
+      route: '/craftnectar',
+      children: [
+        {
+          name: 'Players',
+          icon: 'profile',
+          route: '/playerlist'
+        },
+        {
+          name: 'Leaderboard',
+          icon: 'stats',
+          route: '/leaderboard'
+        }
+      ]
+    }
+  ]
 
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
